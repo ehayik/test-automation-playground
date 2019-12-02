b77311049fccac7a1010b6b439caca6d661f6552
eduardo.eljaiek <eduardo.eljaiek@gmail.com>
2019-12-02 21:52:00 +0200

    refactor(guice-playground): Implement guice provider for `Communication` service

--- guice-playground/src/main/java/com/github/eljaiek/playground/guice/CommModule.java
+++ guice-playground/src/main/java/com/github/eljaiek/playground/guice/CommModule.java
@@ -1,21 +1,36 @@
 package com.github.eljaiek.playground.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.matcher.Matchers;
+import com.google.inject.name.Names;
+import lombok.SneakyThrows;
+import lombok.val;
+
+import javax.inject.Named;
+import java.util.Properties;
 
 public class CommModule extends AbstractModule {
 
     @Override
+    @SneakyThrows
     protected void configure() {
-
         bind(Communicator.class).to(DefaultCommunicatorImpl.class).in(Scopes.SINGLETON);
-        bind(Communication.class).toInstance(new Communication(true));
+        val props = new Properties();
+        props.load(CommModule.class.getResourceAsStream("/application.properties"));
+        Names.bindProperties(binder(), props);
 
         bindInterceptor(
                 Matchers.any(),
                 Matchers.annotatedWith(LogMessage.class),
                 new MessageLoggerInterceptor()
         );
+    }
+
+    @Provides
+    public Communication communication(@Named("communication.keep-records") boolean keepRecords,
+                                       Communicator communicator) {
+        return new Communication(keepRecords, communicator);
     }
 }
